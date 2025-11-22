b190606d113eedd0f994c875ed9a0fac6a0205f7
rimi1605 <dkfladl05@naver.com>
2025-11-22 19:48:32 +0900

    feat: 초기 구조 및 스크립트, 스타일 업로드 (이미지 폴더 제외)

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(function(){
+
+    
+    setInterval(function(){
+        $(".slide ul").delay(2000).animate({marginLeft:"-1640px"}, 400)
+          .delay(2000).animate({marginLeft:"-3280px"}, 400)
+          .delay(2000).animate({marginLeft:"-4920px"}, 400)
+          .delay(2000).animate({marginLeft:"-6560px"}, 400)
+          .delay(2000).animate({marginLeft:"-8200px"}, 400)
+          .delay(2500).animate({marginLeft:"0px"}, 400);
+    }, 14100);
+
+
+
+
+
+
+});
+
+
